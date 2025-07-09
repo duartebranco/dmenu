9dcb120d4ec1179e009f48a66fdee40a6d203f51
duartebranco <duartebranco35@gmail.com>
2025-07-09 17:50:05 +0100

    add highlight colors

--- config.h
+++ config.h
@@ -17,6 +17,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeSelHighlight] = { "#ffc978", "#005577" },
+    [SchemeNormHighlight] = { "#ffc978", "#222222" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
